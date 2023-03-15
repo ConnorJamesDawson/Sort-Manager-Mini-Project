5c07dbc477045da93ee9ae2c21ac6dd3db49c3db
Connor Dawson <Connor.Dawson@hotmail.co.uk>
2023-03-15 11:15:28 +0000

    Common files implemented

---
+++ SortManager/SortManager.App/Model/AbstractSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortManager.App.Model;
+
+public abstract class AbstractSort
+{
+
+    public int[] GetUnsortedArray {get; set;}
+    public int[] GetArray {get; set;}
+    public string GetTime {get; set;}
+
+    public abstract int[] SortArray(int[] array);
+
+    public DateTime StartTimer()
+    {
+        return DateTime.Now;
+    }
+
+    public DateTime StopTimer()
+    {
+        return DateTime.Now;
+    }
+}
