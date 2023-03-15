ff7e7dda8a42ce2bc8f7ba0554ca1d8846e74e95
Connor Dawson <Connor.Dawson@hotmail.co.uk>
2023-03-15 14:47:01 +0000

    Added shell sort

--- SortManager/SortManager.App/Model/MaxRadix.cs
+++ SortManager/SortManager.App/Model/MaxRadix.cs
@@ -8,24 +8,24 @@ namespace SortManager.App.Model
 {
     public class MaxRadix
     {
-        //static public int[] Radixing(int[] countarray, int[] list)
-        //{
-        //    int[] temp = new int[list.Length];
-        //    for (int i = 0; i < countarray.Length; i++)
-        //    {
-        //        if (i == 0) countarray[i] -= 1;
-        //        else
-        //        {
-        //            countarray[i] = countarray[i - 1] + countarray[i];
-        //        }
-        //    }
-        //    for (int i = temp.Length - 1; i > 0; i--)
-        //    {
-        //        temp[countarray[list[i]]] = list[i];
-        //        countarray[list[i]]--;
-        //    }
-        //    return temp;
-        //}
+        static public int[] Radixing(int[] countarray, int[] list)
+        {
+            int[] temp = new int[list.Length];
+            for (int i = 0; i < countarray.Length; i++)
+            {
+                if (i == 0) countarray[i] -= 1;
+                else
+                {
+                    countarray[i] = countarray[i - 1] + countarray[i];
+                }
+            }
+            for (int i = temp.Length - 1; i > 0; i--)
+            {
+                temp[countarray[list[i]]] = list[i];
+                countarray[list[i]]--;
+            }
+            return temp;
+        }
         static public string Radix(int[] list)
         {
             int[] countarray = new int[10];
@@ -47,13 +47,13 @@ namespace SortManager.App.Model
                 temp[countarray[list[i]]] = list[i];
                 countarray[list[i]]--;
             }
-            //list = Radixing(countarray, list);
-            //countarray = new int[10];
-            //for (int i = 0; i < list.Length; i++)
-            //{
-            //    countarray[list[i/10]]++;
-            //}
-            //list = Radixing(countarray, list);
+            list = Radixing(countarray, list);
+            countarray = new int[10];
+            for (int i = 0; i < list.Length; i++)
+            {
+                countarray[list[i/10]]++;
+            }
+            list = Radixing(countarray, list);
             string result = "";
             foreach (int i in list) result += i.ToString() + ",";
             return result;
