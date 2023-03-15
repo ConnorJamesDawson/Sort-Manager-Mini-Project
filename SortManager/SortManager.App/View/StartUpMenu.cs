91aa6d9bb51906847034334cac5efbf0059348dc
Connor Dawson <Connor.Dawson@hotmail.co.uk>
2023-03-15 17:40:42 +0000

    Final Final commit

--- SortManager/SortManager.App/View/StartUpMenu.cs
+++ SortManager/SortManager.App/View/StartUpMenu.cs
@@ -9,13 +9,23 @@ public class StartUpMenu
         Console.WriteLine("Welcome to our Sort Manager");
 
         Console.WriteLine("Please choose a length of your array: ");
-        int lengthInput = Int32.Parse(Console.ReadLine());
-        int[] unsortedArray = HandleInput.GetRandomArray(lengthInput);
+        int lengthInput = 0;
 
-        Console.WriteLine("Please choose from the following sort Methods to sort an unsorted array: \n1: BubbleSort, \n2: HeapSort, \n3: MergeSort \n4: .NET LibrarySort, \n5: RadixSort, \n6: ShellSort");
-        string input = Console.ReadLine().ToLower();
+        if(!Int32.TryParse(Console.ReadLine(), out lengthInput))
+        {
+            Console.WriteLine("Please input a number");
+            StartUpMessage();
+        }
+        else
+        {
+            int[] unsortedArray = HandleInput.GetRandomArray(lengthInput);
 
-        if (input == "quit") Environment.Exit(0);
-        HandleInput.HandleUserInput(input, unsortedArray);
+            Console.WriteLine("Please choose from the following sort Methods to sort an unsorted array: \n1: BubbleSort, \n2: HeapSort, \n3: MergeSort \n4: .NET LibrarySort, \n5: RadixSort, \n6: ShellSort");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "quit") Environment.Exit(0);
+            HandleInput.HandleUserInput(input, unsortedArray);
+        }
+
     }
 }
