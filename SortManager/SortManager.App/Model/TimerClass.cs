1dd0e066ce80b51b8e67b5d9025c6055ba774394
Ruya Kumru-Holroyd <ruya.kh@outlook.com>
2023-03-15 13:37:00 +0000

    Adding timer class and tests

---
+++ SortManager/SortManager.App/Model/TimerClass.cs
@@ -0,0 +1,23 @@
+﻿namespace SortManager.App.Model;
+
+public class TimerClass
+{
+    public static int GetTime(int time) => time;
+    public static int StartTimer(int startTime)
+    {
+        startTime = DateTime.Now.Second;
+        return startTime;
+    }
+
+    public static int StopTimerAndCalculateTime(int startTime)
+    {
+        int timePassed = DateTime.Now.Second - startTime;
+        return timePassed;
+    }
+
+    public override string ToString()
+    {
+        return $"Time taken: {GetTime} seconds";
+    }
+
+}
